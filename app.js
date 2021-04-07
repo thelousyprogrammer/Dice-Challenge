dd2099f9ff2c18cc65cadb9f7077e30718d23171
Yap Wei Jin <dkdwmg@hotmail.com>
2021-04-07 15:48:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express()
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/dicee.html")
+  res.sendFile(__dirname + "/index.html")
 })
 
 app.post("/", function(req, res) {
-  res.sendFile(__dirname + "/dicee.html")
+  res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(process.env.PORT || 3000, function() {
